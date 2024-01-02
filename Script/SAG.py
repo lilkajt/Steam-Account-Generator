ea53e5389c9b654368711fb5c6bf70498f618206
lilkajt <87129505+lilkajt@users.noreply.github.com>
2024-01-02 20:34:03 +0100

    update version 2.2

--- Script/SAG.py
+++ Script/SAG.py
@@ -8,233 +8,260 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from discord import SyncWebhook
+import discord
 import pathlib
 
-chrome_options = Options()
-chrome_options.add_argument('--log-level=3')
-version = '2.1'
-url = "https://store.steampowered.com/join"
-Domain_Input = ''
-clear_line = "cls||clear"
-path = pathlib.Path().resolve()
+ChromeOptions = Options()
+ChromeOptions.add_argument("--log-level=3")
+Version = "2.2"
+Url = "https://store.steampowered.com/join"
+SteamIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/240px-Steam_icon_logo.svg.png"
+BotUrl="https://github.com/lilkajt/Steam-Account-Generator"
+DomainInput = ""
+ClearConsole = os.system("cls||clear")
+BotPath = pathlib.Path().resolve()
 
 # FILE FUNCTIONS
-def data_to_csv(email, username, password):
+def ExportCSV(email, username, password):
     data = {"Email": email, "Username": username, "Password": password}
     df = pd.DataFrame(data, index=[0])
-    file_path = f"{path}\\Created_accounts.csv"
+    filePath = f"{BotPath}\\CreatedAccounts.csv"
 
-    if not os.path.exists(file_path):
-        df.to_csv(file_path, index=False)
+    if not os.path.exists(filePath):
+        df.to_csv(filePath, index=False)
     else:
-        df.to_csv(file_path, mode='a', index=False, header=False)
+        df.to_csv(filePath, mode="a", index=False, header=False)
 
-def read_accounts_from_csv_file():
-    print('\tReading emails from csv file')
-    file_name = input('\tWhat is the file name(without extension): ')
-    emails = pd.read_csv(f"{path}\\{file_name}.csv")
-    return emails['Email'].tolist()
+def AccountsFromCSV():
+    fileName = input("\tWhat is the file name(without extension): ")
+    file = pd.read_csv(f"{BotPath}\\{fileName}.csv")
+    emails = file["Email"].tolist()
+    usernames = file["Username"].tolist()
+    passwords = file["Password"].tolist()
+    return emails, usernames, passwords
 
-def create_new_example_file(file_name):
-    data = {'Email':'Testemail@domain.com'}
+def ExampleFile(fileName):
+    data = {"Email":"Testemail@domain.com", "Username":"test1", "Password":"testpassword"}
     df = pd.DataFrame(data, index=[0])
-    file_path = f"{path}\\{file_name}.csv"
+    filePath = f"{BotPath}\\{fileName}.csv"
 
-    if not os.path.exists(file_path):
-        df.to_csv(file_path, index=False)
+    if not os.path.exists(filePath):
+        df.to_csv(filePath, index=False)
 
-def read_webhook():
+def WebhookURL():
     f = open(f"{pathlib.Path().resolve()}\\webhook.txt", "r")
     return f.readline()
 
-# TIME FUNC
-def sleep(n):
-    time.sleep(n)
-
 # NOTIFICATIONS
-def notification(type_notifi):
-    if type_notifi == "email":
+def Notification(notiType):
+    if notiType == "email":
         key = "m"
-    elif type_notifi == "captcha":
+    elif notiType == "captcha":
         key = "c"
     else:
         key = "n"
-    print(f'\n\tCheck {type_notifi} to confirm!\n\tPress \"{key}\" to continue!')
+    print(f"\n\tCheck {notiType} to confirm!\n\tPress \"{key}\" to continue!")
     while True:
         if BasicBoard.is_pressed(key):
             break
 
 # RANDOM STRINGS
-def random_string(length):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+def RndString(length):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
-def random_password(length):
-    return random_string(length)
+def RndPassword(length):
+    return RndString(length)
 
-def random_domain_email(domain):
-    username = random_string(8)
+def RndDomainEmail(domain):
+    username = RndString(8)
     return f"{username}@{domain}"
 
 # DATA INPUTS
-def launch_option(option):
-    if option == 'domain' or option == 'new_account':
-        email, username, password = data_return(option)
-        create_steam_account(email.lower(), username.lower(), password)
+def GenerateOption(option):
+    if option == "domain":
+        email, username, password = Domain()
+        GenAccount(email, username, password)
+    elif option == "newAccount":
+        email, username, password = OneAccount()
+        GenAccount(email, username, password)
     else:
-        email_list = data_return(option)
-        for i in range(len(email_list)):
+        emails, usernames, passwords = AccountsFromCSV()
+        for i in range(len(emails)):
             print(f"\tGenerating account nr {i+1}")
-            create_steam_account(email_list[i].lower(), email_list[i].split("@")[0].lower(), random_password(14))
+            GenAccount(emails[i], usernames[i], passwords[i])
 
-def data_return(x):
-    if x == "domain":
-        email, username, password = user_domain()
-        return email, username, password
-    elif x == 'new_account':
-        email = input('\tEnter your email: ')
+def OneAccount():
+        email = input("\tEnter your email: ")
         username = email.split("@")[0]
-        password = random_password(14)
+        password = RndPassword(14)
         return email, username, password
-    else:
-        emails_list = read_accounts_from_csv_file()
-        return emails_list
 
-def user_domain():
-    global Domain_Input
-    if Domain_Input == '':
-        Domain_Input = input("\tInput domain: ")
-        email = random_domain_email(Domain_Input)
+def Domain():
+    global DomainInput
+    if DomainInput == "":
+        DomainInput = input("\tEnter your domain: ")
+        email = RndDomainEmail(DomainInput)
     else:
-        email = random_domain_email(Domain_Input)
+        email = RndDomainEmail(DomainInput)
     username = email.split("@")[0]
-    password = random_password(14)
+    password = RndPassword(14)
     return email, username, password
 
 # WEBHOOK
-def send_to_webhook(email, username, password):
-    webhook_url = read_webhook()
-    if webhook_url == 'INSTEAD OF THIS LINE PUT YOUR DISCORD WEBHOOK TO GET NOTIFICATIONS!' or webhook_url == "":
-        print(f"\tInvalid webhook, change in webhook.txt if you would like to.\n\tContinue...")
+def WebhookSend(email, username, password, profLink):
+    webhookUrl = WebhookURL()
+    if webhookUrl == "INSTEAD OF THIS LINE PUT YOUR DISCORD WEBHOOK TO GET NOTIFICATIONS!" or webhookUrl == "":
+        print("\tInvalid webhook, change in webhook.txt.\n\tContinue...")
     else:
-        webhook = SyncWebhook.from_url(webhook_url)
-        webhook.send(f"Steam account created\nEmail: {email}\nUsername: {username}\nPassword: {password}")
+        webhook = discord.SyncWebhook.from_url(webhookUrl)
+        webhook.send(embed=DiscordEmbed(email, username, password, profLink))
+
+def DiscordEmbed(email, username, password, profLink):
+    embed = discord.Embed(
+        title="New Steam Account",
+        color=0x669900
+    )
+    embed.set_author(name="SAG - Steam Account Generator", url=BotUrl)
+    embed.add_field(name="Username", value=f"{username}")
+    embed.add_field(name="Password", value=f"||{password}||")
+    embed.add_field(name="Email", value=f"{email}",inline=False)
+    embed.add_field(name="Profile link", value=f"{profLink}",inline=False)
+    embed.set_footer(text="Account generated using SAG bot by Lilkajt", icon_url=SteamIcon)
+    return embed
 
 # MESSAGE
-def welcome_message():
-    print('')
-    print('--------------------------------------------------')
-    print(f'| WELCOME TO STEAM-ACCOUNT-GENERATOR VERSION {version} |')
-    print('--------------------------------------------------')
-    print('')
-    list_message = [[1, "create accounts from file"], [2, "create new account each time"], [3, "create accounts using your domain"], [4, "Create example file"], [5, "HELP"], [6, "Contact info"]]
+def MenuMessage():
+    print("")
+    print("--------------------------------------------------")
+    print(f"| WELCOME TO STEAM-ACCOUNT-GENERATOR Version {Version} |")
+    print("--------------------------------------------------")
+    print("")
+    messageList = [[1, "create accounts from file"], [2, "create new account each time"], [3, "create accounts using your domain"], [4, "Create example file"], [5, "HELP"], [6, "Contact info"]]
     column = ["Options", "Description"]
-    df = pd.DataFrame(list_message, columns=column)
+    df = pd.DataFrame(messageList, columns=column)
     print(df.to_string(index=False))
 
 def Help():
-    # advanced descriptions of functions NEEDS TO BE DONE
-    print('\n\tHELP')
-    print('\tOption domain supports custom user domains')
+    print("\n\tHELP")
+    print("\tOption domain supports custom user domains")
 
 def Contact():
-    print('\n\tContact information')
-    print('\tdiscord: lilkajt#6121')
+    print("\n\tContact information")
+    print("\tdiscord: lilkajt#6121")
 
 # GENERATOR
-def create_steam_account(email, username, password):
-    sleep(np.random.uniform(0, 0.1))
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
-    sleep(6)
+def GenAccount(email, username, password):
+    time.sleep(np.random.uniform(0, 0.1))
+    driver = webdriver.Chrome(options=ChromeOptions)
+    driver.get(Url)
+    time.sleep(6)
     driver.find_element(By.XPATH, '//*[@id="email"]').click()
-    for x in email: driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(x), sleep(np.random.uniform(0, 0.1))
-    sleep(0.5)
+    for x in email: driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(x), time.sleep(np.random.uniform(0, 0.1))
+    time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="reenter_email"]').click()
-    for x in email: driver.find_element(By.XPATH, '//*[@id="reenter_email"]').send_keys(x), sleep(np.random.uniform(0, 0.1))
-    sleep(0.5)
+    for x in email: driver.find_element(By.XPATH, '//*[@id="reenter_email"]').send_keys(x), time.sleep(np.random.uniform(0, 0.1))
+    time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="i_agree_check"]').click()
-    notification("captcha")
+    Notification("captcha")
     print("\n\tCaptcha confirmed!")
     driver.find_element(By.XPATH, '//*[@id="createAccountButton"]').click()
-    notification("email")
+    Notification("email")
     print("\n\tEmail confirmed!")
-    sleep(2)
+    SameEmail(driver)
+    time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="accountname"]').click()
-    for x in username: driver.find_element(By.XPATH, '//*[@id="accountname"]').send_keys(x), sleep(np.random.uniform(0, 0.1))
-    sleep(0.5)
+    for x in username: driver.find_element(By.XPATH, '//*[@id="accountname"]').send_keys(x), time.sleep(np.random.uniform(0, 0.1))
+    time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="password"]').click()
-    for x in password: driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(x), sleep(np.random.uniform(0, 0.1))
-    sleep(0.5)
+    for x in password: driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(x), time.sleep(np.random.uniform(0, 0.1))
+    time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="reenter_password"]').click()
-    for x in password: driver.find_element(By.XPATH, '//*[@id="reenter_password"]').send_keys(x), sleep(np.random.uniform(0, 0.1))
-    username = name_check(driver, username)
-    sleep(2)
+    for x in password: driver.find_element(By.XPATH, '//*[@id="reenter_password"]').send_keys(x), time.sleep(np.random.uniform(0, 0.1))
+    username = NameCheck(driver, username)
+    time.sleep(1)
     driver.find_element(By.XPATH, '//*[@id="createAccountButton"]').click()
-    sleep(7)
+    time.sleep(6)
+    profLink = ExtractLink(driver)
     print("\tAccount successfully created")
-    data_to_csv(email, username, password)
-    send_to_webhook(email, username, password)
+    ExportCSV(email, username, password)
+    WebhookSend(email, username, password, profLink)
     driver.close()
 
-def name_check(driver, og_username):
-    if driver.find_element(By.ID, 'accountname_availability').text == "Not Available":
-        sleep(1)
-        driver.find_element(By.ID, "suggested_name_1").click()
-        username = driver.find_element(By.XPATH, '//*[@id="suggested_name_1"]').text
-        print(f"\tName taken. Changing name.\n\tNew name {username}")
-        notification("account name")
-        return username
-    return og_username
+def SameEmail(driver,option=True):
+    element = f"return document.querySelectorAll(\"button\")[2].textContent"
+    element = driver.execute_script(element)
+    if element == "Continue" and option == True:
+        driver.execute_script("EmailConfirmedVerified( 0 );")
+    else:
+        print("\tWaiting for input...")
+        input()
 
-# start
-def program_start():
-    os.system(clear_line)
-    welcome_message()
+def NameCheck(driver, username):
+    element = "return document.querySelectorAll(\".password_tag.warning\")[0].textContent"
+    element = driver.execute_script(element)
+    # print(f"name check script run element {element}")
+    if element == "Not Available":
+        time.sleep(1)
+        newUsername = driver.execute_script("return document.querySelector(\"#suggested_name_1\").text")
+        # newUsername = driver.find_element(By.XPATH, '//*[@id="suggested_name_1"]').text
+        driver.find_element(By.ID, "suggested_name_1").click()
+        print(f"\tName taken. Changing name.\n\tNew name {newUsername}")
+        Notification("account name")
+        return newUsername
+    return username
+
+def ExtractLink(driver):
+    profLink = f"return document.querySelector(\"a\").getAttribute(\"href\")"
+    profLink = driver.execute_script(profLink)
+    return profLink
+
+def ProgramMenu():
+    ClearConsole
+    MenuMessage()
     match (input("Input number you choose: ")):
         case "1":
-            os.system(clear_line)
-            print('\tYou choose creating accounts from file')
-            launch_option("file_accounts")
+            ClearConsole
+            print("\tYou choose creating accounts from CSV file")
+            GenerateOption("file_accounts")
         case "2":
-            os.system(clear_line)
-            print('\tYou choose creating new accounts each time')
-            for i in range(int(input('\tHow many accounts would you like to generate? '))):
+            ClearConsole
+            print("\tYou choose creating new accounts each time")
+            for i in range(int(input("\tHow many accounts would you like to generate? "))):
                 print(f"\tGenerating account nr {i+1}")
-                launch_option("new_account")
+                GenerateOption("newAccount")
         case "3":
-            os.system(clear_line)
-            print('\tYou choose creating accounts using your domain\t')
-            for i in range(int(input('\tHow many accounts would you like to generate? '))):
+            ClearConsole
+            print("\tYou choose creating accounts using your domain\t")
+            for i in range(int(input("\tHow many accounts would you like to generate? "))):
                 print(f"\tGenerating account nr {i+1}")
-                launch_option("domain")
+                GenerateOption("domain")
         case "4":
-            os.system(clear_line)
-            print('\tCreating example file')
-            create_new_example_file(input("\tWhat file name do you want: "))
-            print('\tDone! Check current folder')
-            sleep(3)
-            program_start()
+            ClearConsole
+            print("\tCreating example file")
+            ExampleFile(input("\tWhat file name do you want: "))
+            print("\tDone! Check current folder")
+            time.sleep(3)
+            ProgramMenu()
         case "5":
-            os.system(clear_line)
+            ClearConsole
             Help()
-            sleep(5)
-            program_start()
+            time.sleep(5)
+            ProgramMenu()
         case "6":
-            os.system(clear_line)
+            ClearConsole
             Contact()
-            sleep(5)
-            program_start()
+            time.sleep(5)
+            ProgramMenu()
         case other:
             print(f"Incorrect input! {other}")
-            print('Restarting!')
-            sleep(5)
-            os.system(clear_line)
-            program_start()
+            print("Restarting!")
+            time.sleep(5)
+            ClearConsole
+            ProgramMenu()
 
 try:
-    program_start()
-    sleep(1)
+    ProgramMenu()
+    time.sleep(1)
 except Exception as err:
     print(err)
     print("Press any key to close the program...")
